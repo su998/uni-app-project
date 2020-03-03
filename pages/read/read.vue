41ec9483f5e5defaf49c12a4bd55ee783c73b5ae
su998 <951193203@qq.com>
2020-03-03 18:10:11 +0800

    阅读界面和精选顶部导航轮播

--- pages/read/read.vue
+++ pages/read/read.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="read">
+		<!-- 精选期刊 -->
 		<view class="featured_journals">
 			<view class="featured_journals_header">
 				<text class="title">精选期刊</text>
@@ -14,7 +15,23 @@
 				<view class="featured_journal_item"></view>
 				<view class="featured_journal_item"></view>
 				<view class="featured_journal_item"></view>
-				<view class="featured_journal_item"></view>
+			</view>
+		</view>
+		<!-- 热门听书 -->
+		<view class="hot_listenBook">
+			<view class="hot_listenBook_header">
+				<text class="title">热门听书</text>
+				<text class="more">查看更多 ></text>
+			</view>
+			<view class="hot_listenBook_container">
+				<view class="hot_listenBook_item"></view>
+				<view class="hot_listenBook_item"></view>
+				<view class="hot_listenBook_item"></view>
+				<view class="hot_listenBook_item"></view>
+				<view class="hot_listenBook_item"></view>
+				<view class="hot_listenBook_item"></view>
+				<view class="hot_listenBook_item"></view>
+				<view class="hot_listenBook_item"></view>
 			</view>
 		</view>
 	</view>
@@ -38,7 +55,7 @@
 			display: flex;
 			align-items: flex-end;
 			justify-content: space-between;
-			border-bottom: 1px solid red;
+			margin-bottom: 30rpx;
 			.title {
 				font-family: 'PingFang-SC-Bold';
 				font-weight: bold;
@@ -53,13 +70,46 @@
 		.featured_journals_container {
 			width: 100%;
 			display: flex;
-			flex-flow: row wrap;
+			flex-flow: wrap;
 			.featured_journal_item {
 				width: 150rpx;
 				height: 200rpx;
 				background-color: #ccc;
 			}
-			.featured_journal_item:not(:first-child) {
+			.featured_journal_item:not(:nth-child(4n)) {
+				margin-right: 30rpx;
+				margin-bottom: 30rpx;
+			}
+		}
+	}
+	// 热门听书
+	.hot_listenBook {
+		.hot_listenBook_header{
+			display: flex;
+			align-items: flex-end;
+			justify-content: space-between;
+			margin-bottom: 30rpx;
+			.title {
+				font-family: 'PingFang-SC-Bold';
+				font-weight: bold;
+				font-size: 32rpx;
+			}
+			.more {
+				font-family: 'PingFang-SC-Medium';
+				color: #999999;
+				font-size: 24rpx;
+			}
+		}
+		.hot_listenBook_container {
+			width: 100%;
+			display: flex;
+			flex-flow: wrap;
+			.hot_listenBook_item {
+				width: 150rpx;
+				height: 200rpx;
+				background-color: #ccc;
+			}
+			.hot_listenBook_item:not(:nth-child(4n)) {
 				margin-right: 30rpx;
 				margin-bottom: 30rpx;
 			}
